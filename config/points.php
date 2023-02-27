d0d17afd366ed12c698c0e882d42278a3a28b7e6
gonzaz704 <gonzaz704@gmail.com>
2023-02-26 21:29:45 -0300

    Add files via upload

---
+++ config/points.php
@@ -0,0 +1,21 @@
+<?php 
+
+return [
+    'data' => 1,
+    'invitation' => 5,
+    'published' => 10,
+    'paper' => 30,
+    'review' => 20,
+
+    'tag' => [
+        'User' => [
+            100 => 'VC',
+            50 => 'C',
+            'default' => 'User'
+        ],
+        'Author' => [
+            500 => 'VA',
+            'default' => 'Author'
+        ]
+    ]
+];
